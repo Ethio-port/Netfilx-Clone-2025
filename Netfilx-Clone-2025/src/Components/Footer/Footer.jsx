108a14b99357551b5497546cfec614fc51b399d6
Ethio-port <yenesew2003@yahoo.com>
2024-11-18 12:36:49 -0800

    banner

--- Netfilx-Clone-2025/src/Components/Footer/Footer.jsx
+++ Netfilx-Clone-2025/src/Components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import "./Footer.css";
@@ -7,47 +8,46 @@ const Footer = () => {
     <div className="footer_outer_container">
       <div className="footer_inner_container">
         <div className="footer_icons">
-          <facebookOutlinedIcon />
+          <FacebookOutlinedIcon />
           <InstagramIcon />
           <YouTubeIcon />
         </div>
-        <div className="footer_data"></div>
-        <ul>
-          <li>Audio Description</li>
-          <li>Investor Relations</li>
-          <li>Legal Notice</li>
-        </ul>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie preference</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of User</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Use</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="service_code">
+          <p>Service Code</p>
+        </div>
+        <div className="copy_write"> &copy;1997-2024Netflix,Inc</div>
       </div>
-      <div>
-        <ul>
-          <li>Help Center</li>
-          <li>Jobs</li>
-          <li>Cookie preference</li>
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li>Gift Cards</li>
-          <li>Terms of User</li>
-          <li>Corporate Information</li>
-        </ul>
-      </div>
-      {/* <div> */}
-      {/* <ul>
-          <li>Gift Cards</li>
-          <li>Terms of Users</li>
-          <li>Corporate Information</li>
-        </ul> */}
-      {/* </div> */}
-      <div>
-        <ul>
-          <li>Media Center</li>
-          <li>Privacy</li>
-          <li>Contact Use</li>
-        </ul>
-      </div>
-      <div className="service_code">{/* <P>Service Code</P> */}</div>
-      <div className="copy write"> &copy;1997-2024Netflix,Inc</div>
     </div>
   );
 };
