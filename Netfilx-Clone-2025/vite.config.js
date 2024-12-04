4b6003c68faa976a8771727a40984b72fa6db79f
Ethio-port <yenesew2003@yahoo.com>
2024-12-03 16:40:28 -0800

    provided project path

--- Netfilx-Clone-2025/vite.config.js
+++ Netfilx-Clone-2025/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "netfilx-clone-2025",
-  outDit:'build'
+  base: "/Netfilx-Clone-2025/",
+  
   
 });
